c2ca83516d86c4503b6704d16876ecc647fdd4cc
ahmed shaheen <3126783707@91-73_R101->
2025-01-15 11:11:04 +0300

    done

---
+++ BlazorApp1/ViewModels/EditUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.ViewModels
+{
+    public class EditUserDTO
+    {
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Email Required")]
+        [DataType(DataType.EmailAddress,ErrorMessage ="Invalid Email")]
+        public string? Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Full Name Required")]
+        public string? FullName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Required")]
+        [DataType(DataType.PhoneNumber,ErrorMessage = "Invalid Phone Number")]
+        public string? Phone { get; set; }
+    }
+}
